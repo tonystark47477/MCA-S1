6af987cca7ec626ff9349dc923ce6302779bcb59
Melbin K Madhu <123544222+tonystark47477@users.noreply.github.com>
2025-11-22 18:34:51 +0530

    Add files via upload

---
+++ WP LAB/students_array.php
@@ -0,0 +1,12 @@
+<?php 
+$a = array("Anjala", "Christeena", "Blessey", "Angela"); 
+print_r($a);
+
+echo "<br>Ascending order : ";
+asort($a);
+print_r($a);
+
+echo "<br>Descending order : ";
+arsort($a);
+print_r($a);
+?>
